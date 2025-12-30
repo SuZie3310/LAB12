4bbfcc5164ab17d47824d01769d11bb9d779054c
SuZie3310 <Surapat5163@gmail.com>
2025-12-30 15:17:39 +0700

    2

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,14 +1,16 @@
-#include<iostream>
-using namespace std;
-int fibonacci(int x);
+#include<iostream> 
+#include<ctime> 
+using namespace std; 
+long long int fibonacci(int x); 
 
-int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
+int main(){ clock_t start = clock(); 
+    cout << "Result: " << fibonacci(50) << "\n"; 
+    clock_t end = clock(); 
+    double elapsed = double(end - start)/CLOCKS_PER_SEC; 
+    cout << "Elapsed Time: " << elapsed << " seconds.";
     return 0; 
-}
-int fibonacci(int x){
-    if(x <= 1) return x;
-    return fibonacci(x-1) + fibonacci(x-2);
+    } 
+long long int fibonacci(int x){ 
+    if(x <= 1) return x; 
+    return fibonacci(x-1) + fibonacci(x-2); 
 }
